79e7c664342823214dc9ff84408d8d3ed84595db
monkeydunkey <bhushan.shashank93@gmail.com>
2017-01-02 12:49:10 -0700

    adding basic queue implementation

---
+++ python/queue.py
@@ -0,0 +1,35 @@
+## Queue implementation using singly linked list
+
+import linkedList as ll
+
+class queue(object):
+    def __init__ (self):
+        self.ptr = None;
+        self.head = None;
+
+    def enqueue(self, value):
+        if self.head is None:
+            self.head = ll.singleNode(value)
+            self.ptr = self.head
+        else:
+            self.ptr.next = ll.singleNode(value);
+            self.ptr = self.ptr.next;
+
+    def dequeue(self):
+        retValue = None
+        if self.ptr is None:
+            return 'Queue is empty'
+        else:
+            retValue = self.ptr.value
+            tmp = self.head
+            if tmp is self.ptr:
+                self.ptr = None
+                self.head = None
+            else:
+                while tmp.next is not self.ptr:
+                    tmp = tmp.next
+                    continue
+                tmp.next = None
+                self.ptr = tmp
+
+            return retValue
