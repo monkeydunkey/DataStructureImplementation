b29256f758ef5f32c0eb44089019ba71c051ba75
monkeydunkey <bhushan.shashank93@gmail.com>
2017-01-02 12:12:56 -0700

    added doubly linked implementation

--- python/linkedList.py
+++ python/linkedList.py
@@ -1,7 +1,19 @@
 ## Linked list implementation in python
-class Node(object):
-    def __init__(self, value=None, next=None):
+
+## Implementation of singly linked, linked list
+class singleNode(object):
+    def __init__(self, value = None, next = None):
         self.value = value;
         self.next = next;
     def __str__(self):
-        return str(self.car)
+        return str(self.value)
+
+## Implementation of doubly linked, linked list 
+class doubleNode(object):
+    def __init__(self, value = None, next = None, prev = None):
+        self.value = value;
+        self.next = next;
+        self.prev = prev;
+
+    def __str__(self):
+        return str(self.value)
