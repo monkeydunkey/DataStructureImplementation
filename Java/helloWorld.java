79204c935a0b0d195c147c1a37d1eec2a97569a2
monkeydunkey <bhushan.shashank93@gmail.com>
2017-02-24 22:42:54 -0700

    Few java implementation

---
+++ Java/helloWorld.java
@@ -0,0 +1,11 @@
+class helloWorld{
+ public static void main(String args[]){
+   System.out.println("Hello World!!");
+ }
+}
+
+class simple{
+ public static void main(String[] args){
+  System.out.println("Hello World from Simple!!");
+ }
+}
